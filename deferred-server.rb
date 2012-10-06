521f25c961dd404fe79c6a0bc1cfe8cc7b74d1f0
Dan Mayer <dan@mayerdan.com>
2012-10-06 00:39:25 -0400

    refactored into server module and post to server

--- deferred-server.rb
+++ deferred-server.rb
@@ -4,42 +4,6 @@ require 'rest-client'
 
 DEFAULT_AMI = ENV['WAKE_UP_AMI'] || 'ami-0267bb6b'
 
-def find_server
-  compute = Fog::Compute.new(
-                             :provider          => 'AWS',
-                             :aws_access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
-                             :aws_secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY'])
-
-  server = compute.servers.detect{ |server| server.image_id==DEFAULT_AMI && server.ready? }
-  server ||= compute.servers.detect{ |server| server.image_id==DEFAULT_AMI }
-
-  if server.nil?
-    puts "creating new server"
-    server = compute.servers.create(:image_id => DEFAULT_AMI,
-                                    :name => 'wakeup-hook-responder')
-  end
-  server
-end
-
-def start_server
-  server = find_server
-
-  if server && !server.ready?
-    puts "starting server"
-    server.start
-  end
-
-  server.wait_for { ready? }
-
-  puts "server is ready"
-  server
-end
-
-def stop_server
-  server = find_server
-  server.stop
-end
-
 # Run me with 'ruby' and I run as a script
 if $0 =~ /#{File.basename(__FILE__)}$/
   puts "running as local script"
@@ -49,8 +13,10 @@ if $0 =~ /#{File.basename(__FILE__)}$/
   server_ip = server.public_ip_address
   #server_ip = "127.0.0.1:3000"
 
+  push = {:test => 'fake'}
+
   puts "server is at #{server_ip}"
-  response = RestClient.post "http://#{server_ip}", :data => {:test => 'data'}.to_json, :content_type => :json, :accept => :json
+  response = post_to_server(:payload, push, {:server => server, :server_ip => server_ip})
   puts response.inspect
   #stop_server
 
@@ -70,8 +36,7 @@ else
     server_ip = server.public_ip_address
 
     #post payload to ec2 server
-    response = RestClient.post "http://#{server_ip}", :payload => push.to_json, :content_type => :json, :accept => :json
-    puts response.inspect
+    response = post_to_server(:payload, push, {:server => server, :server_ip => server_ip})
   end
 
 end
---
+++ lib/server-commands.rb
@@ -0,0 +1,45 @@
+module
+
+  def find_server
+    compute = Fog::Compute.new(
+                               :provider          => 'AWS',
+                               :aws_access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
+                               :aws_secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY'])
+
+    server = compute.servers.detect{ |server| server.image_id==DEFAULT_AMI && server.ready? }
+    server ||= compute.servers.detect{ |server| server.image_id==DEFAULT_AMI }
+
+    if server.nil?
+      puts "creating new server"
+      server = compute.servers.create(:image_id => DEFAULT_AMI,
+                                      :name => 'wakeup-hook-responder')
+    end
+    server
+  end
+
+  def start_server
+    server = find_server
+
+    if server && !server.ready?
+      puts "starting server"
+      server.start
+    end
+
+    server.wait_for { ready? }
+
+    puts "server is ready"
+    server
+  end
+
+  def stop_server
+    server = find_server
+    server.stop
+  end
+
+  def post_to_server(wrapper, package, options = {})
+    server = options.fetch(:server){ find_server }
+    server_ip = options.fetch(:server_ip){ server.public_ip_address }
+    response = RestClient.post "http://#{server_ip}", wrapper => package.to_json, :content_type => :json, :accept => :json
+  end
+
+end
