e5c6eaf7e72c280765520653b66c35402b535b21
Dan Mayer <dan@mayerdan.com>
2012-11-25 16:34:00 -0500

    adding ruby plugin test

---
+++ public/javascript/code-runner.js
@@ -0,0 +1,72 @@
+(function ($) {
+    'use strict';
+    var pluginName = 'exampleRunner',
+        defaults = {
+            propertyName: "value"
+        };
+
+    function Plugin(element, options) {
+        this.element = element;
+        this.options = $.extend({}, defaults, options);
+        var currentPluggin = this;
+
+        this.runExample = function () {
+	  var payload_signature = $(element).data('sig');
+          var script_payload = $(element).find('code').text();
+	  var script_package = {
+	    signature: payload_signature,
+	    script_payload: script_payload
+	  };
+
+	  $.post('/deferred_code', script_package, function(data){
+	    console.log('received future result: ' + data);
+	    result_future_data = $.parseJSON(data);
+	    if(result_future_data['results_location']) {
+	      results_location = result_future_data['results_location']
+	      currentPluggin.getFutureResult(results_location);
+	    }
+	  });
+
+        };
+
+      this.getFutureResult = function(results_location) {
+	$.getJSON(results_location, function(data) {
+	  if(data['not_complete']) {
+            console.log('data not ready trying again');
+            setTimeout('currentPluggin.getFutureResult(results_location);', 3000);
+	  } else {
+	    if(data && data['results']) {
+	      $(element).append('<div class="results-container"><div>results:</div><pre class="run-results"></pre></div>');
+              $('.run-results').html(data['results']);
+	    }
+	  }
+	});
+      };
+
+        this.addRunButton = function () {
+          $(element).append('<input type="submit" class="run-button" name="runner" value="run"></input>');
+	  $('.run-button').click(function() {
+	    currentPluggin.runExample();
+	    return false;
+	  });
+	};
+
+        this.init();
+        return this;
+    }
+
+    Plugin.prototype.init = function () {
+      this.addRunButton();
+    };
+
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+            }
+        });
+    };
+
+}(jQuery));
+
+$('.ruby-runner').exampleRunner({});
