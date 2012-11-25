329964002a0631e1d332433c2683fa472722e34e
Dan Mayer <dan@mayerdan.com>
2012-11-25 17:33:38 -0500

    results jsonp

--- deferred-server.rb
+++ deferred-server.rb
@@ -65,9 +65,9 @@ else
   get '/results/*' do |results_future|
     results = get_file(results_future)
     if results && results!=''
-      jsonp {:results => results}.to_json
+      jsonp ({:results => results}.to_json)
     else
-      jsonp {:not_complete => true}.to_json
+      jsonp ({:not_complete => true}.to_json)
     end
   end
 
