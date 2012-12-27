e466100aaa3aaff292e13981e93018808ca32497
Dan Mayer <dan@mayerdan.com>
2012-12-27 11:49:05 -0500

    rewritten as jquery map

--- public/javascript/code-runner.js
+++ public/javascript/code-runner.js
@@ -86,7 +86,9 @@
 
 	    if(parsed_data && parsed_data['results']) {
 	      $(element).append('<div class="file-results-container"><div>files:</div><ul class="file-results"></ul></div>');
-	      $(parsed_data['results']).each(function(index) { $('.file-results').append('<li><a href="'+this+'">'+this+'</a></li>'); })
+	      $.map(parsed_data['results'], function(index) {
+		$('.file-results').append('<li><a href="'+this+'">'+this+'</a></li>');
+	      });
 	    } else {
 	      alert('no files!');
 	    }
