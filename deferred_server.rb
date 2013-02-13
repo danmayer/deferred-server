d67b2ad3688fce333969ceba16063837d262008f
Dan Mayer <dan@mayerdan.com>
2013-02-12 20:13:46 -0500

    increased the trusted GH ips

--- deferred_server.rb
+++ deferred_server.rb
@@ -18,7 +18,7 @@ API_KEY = ENV['SERVER_RESPONDER_API_KEY']
 #https://github.com/danmayer/deferred-server/settings/hooks
 TRUSTED_IPS   = ['207.97.227.253', '50.57.128.197',
                  '108.171.174.178', '127.0.0.1',
-                 '50.57.231.61']
+                 '50.57.231.61', '54.235.183.49']
 
 # Run me with 'ruby' and I run as a script
 if $0 =~ /#{File.basename(__FILE__)}$/
