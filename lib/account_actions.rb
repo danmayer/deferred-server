4ed7ace059d3f70f52739764f9f4ac0bad47a2e6
Dan Mayer <dan@mayerdan.com>
2013-03-01 22:47:46 -0500

    record scripts submitted

--- lib/account_actions.rb
+++ lib/account_actions.rb
@@ -1,54 +1,56 @@
 module DeferredServer
   class App < Sinatra::Base
 
-  get '/account' do
-    authenticate!
-    @server_state = find_server.state
-    @projects = get_projects_by_user(github_user.login)
-    erb :account
-  end
+    get '/account' do
+      authenticate!
+      @server_state = find_server.state
+      @projects = get_projects_by_user(github_user.login)
+      erb :account
+    end
 
-  get '/unauthenticated' do
-    @server_state = find_server.state
-    @projects = {}
-    @error = "Error logging in try again"
-    erb :index
-  end
+    get '/unauthenticated' do
+      @server_state = find_server.state
+      @projects = {}
+      @error = "Error logging in try again"
+      erb :index
+    end
 
-  get '/servers' do
-    authenticate!
-    @server = find_server
-    @projects = get_projects_by_user(github_user.login)
-    erb :servers
-  end
+    get '/servers' do
+      authenticate!
+      @server = find_server
+      @projects = get_projects_by_user(github_user.login)
+      erb :servers
+    end
 
-  after '/signed_script' do
-    session['user_script'] = nil
-    session['signature'] = nil
-  end
+    after '/signed_script' do
+      session['user_script'] = nil
+      session['signature'] = nil
+    end
 
-  get '/signed_script' do
-    authenticate!
-    @server_state = find_server.state
-    @projects = get_projects_by_user(github_user.login)
-    @signature = session['signature'] || nil
-    @user_script = session['user_script'] || "puts 'enter ruby code here'"
-    erb :signed_script
-  end
-  
-  post '/sign_script' do
-    authenticate!
-    code = params['user_script']
-    signature = code_signature(code)
-    session['user_script'] = code
-    session['signature'] = signature
-    redirect "/signed_script"
-  end
+    get '/signed_script' do
+      authenticate!
+      @server_state = find_server.state
+      @projects = get_projects_by_user(github_user.login)
+      @signature = session['signature'] || nil
+      @user_script = session['user_script'] || "puts 'enter ruby code here'"
+      erb :signed_script
+    end
+    
+    post '/sign_script' do
+      authenticate!
+      code = params['user_script']
+      signature  = code_signature(code)
+      script_key = "#{github_user.login}/scripts/#{signature}"
+      write_file(script_key, code)
+      session['user_script'] = code
+      session['signature'] = signature
+      redirect "/signed_script"
+    end
 
-  get '/logout' do
-    logout!
-    redirect "/"
-  end
+    get '/logout' do
+      logout!
+      redirect "/"
+    end
 
   end
 end
