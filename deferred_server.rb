f8c0850ff071e579949091bd1e6231671744a1be
Dan Mayer <dan@mayerdan.com>
2013-08-26 19:29:32 -0400

    typo

--- deferred_server.rb
+++ deferred_server.rb
@@ -22,7 +22,7 @@ else
       if ENV['RACK_ENV'] == "production"
         Airbrake.configure do |config|
           config.api_key = ENV['ERRBIT_API_KEY']
-          config.host    = ENV['ERRBIT_API_KEY']
+          config.host    = ENV['ERRBIT_HOST']
           config.port    = 80
           config.secure  = config.port == 443
         end
